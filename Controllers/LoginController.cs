b43a03aae84cb9031d3c1388ffe51a9e18bb0d5a
Chih hua Chou <royloveu@gmail.com>
2025-03-13 16:24:06 +0800

    0313會員登入(含註冊、修改)完成

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
 using System.Reflection.Metadata;
 using NuGet.Protocol;
+using iStudyTest.Filters;
 
 namespace iStudyTest.Controllers
 {
@@ -33,7 +34,7 @@ namespace iStudyTest.Controllers
                 //發給證明,證明他已經登入
                 HttpContext.Session.SetString("MemberInfo", result.ToJson());
 
-                return RedirectToAction("Index", "ProductList");
+                return RedirectToAction("Index","Home");
             }
             else //如果帳密不正確,回到登入頁面,並告知帳密錯誤
             {
@@ -46,7 +47,7 @@ namespace iStudyTest.Controllers
         {
             //5.4.2 在Logout Action中清除Session
             HttpContext.Session.Remove("MemberInfo");//清掉
-            return RedirectToAction("Index", "ProductsPreview");
+            return RedirectToAction("Index", "Home");
         }
         public IActionResult Register()
         {
@@ -57,9 +58,9 @@ namespace iStudyTest.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(Member member)
         {
-            ModelState.Remove("MemberID");
+            ModelState.Remove("MemberID");  //移除MemberID的驗證，使用DB預存程序產生
 
-            member.Password = _context.ComputeSha256Hash(member.Password);
+            member.Password = _context.ComputeSha256Hash(member.Password); //將密碼雜湊
 
             if (ModelState.IsValid)
             {
@@ -74,13 +75,67 @@ namespace iStudyTest.Controllers
             return View(member);
         }
 
-        public bool MemberAccountCheck(string email)
+        [ServiceFilter(typeof(LoginStatusFilter))]
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Members/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MemberID,Name,Birthday,Address,Gender,Email,phone,Password")] Member member)
+        {
+            if (id != member.MemberID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(member);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MemberExists(member.MemberID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+        public bool MemberEmailCheck(string email)
         {
 
             var result = _context.Member.Where(m => m.Email == email).FirstOrDefault();
-            Task.Delay(1000).Wait();
+            //Task.Delay(500).Wait();
 
             return result == null;
+        }
+
+        private bool MemberExists(string id)
+        {
+            return _context.Member.Any(e => e.MemberID == id);
         }
 
     }
